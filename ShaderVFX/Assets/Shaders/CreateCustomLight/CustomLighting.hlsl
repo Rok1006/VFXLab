a9d98ccfcb3b6dd7815f73197903629bf4009093
ROK <hilary0310@gmail.com>
2023-06-04 00:26:25 -0400

    baby step (if fail return to last fml)

--- ShaderVFX/Assets/Shaders/CreateCustomLight/CustomLighting.hlsl
+++ ShaderVFX/Assets/Shaders/CreateCustomLight/CustomLighting.hlsl
@@ -31,19 +31,19 @@ void MainLight_float (float3 WorldPos, out float3 Direction, out float3 Color,
 #endif
  
 }
-// void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
+// void MainLight_half(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
 // {
-//     #ifdef SHADERGRAPH_PREVIEW
+//     #ifndef SHADERGRAPH_PREVIEW
 //         Direction = half3(0.5, 0.5, 0);
 //         Color = 1;
 //         DistanceAtten = 1;
 //         ShadowAtten = 1;
 //     #else
 //         #if SHADOWS_SCREEN
-//             half4 clipPos = TransformWorldToHClip(WorldPos);
-//             half4 shadowCoord = ComputeScreenPos(clipPos);
+//             float4 clipPos = TransformWorldToHClip(WorldPos);
+//             float4 shadowCoord = ComputeScreenPos(clipPos);
 //         #else
-//             half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+//             float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
 //         #endif
 //             Light mainLight = GetMainLight(shadowCoord);
 //             Direction = mainLight.direction;
@@ -58,7 +58,7 @@ void MainLight_float (float3 WorldPos, out float3 Direction, out float3 Color,
 //             ShadowAtten = SampleScreenSpaceShadowmap(shadowCoord);
 //         #else
 //             ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-//             half shadowStrength = GetMainLightShadowStrength();
+//             float shadowStrength = GetMainLightShadowStrength();
 //             ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
 //             sampler_MainLightShadowmapTexture),
 //             shadowSamplingData, shadowStrength, false);
